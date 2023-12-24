b091ee1713535c6c1837af8b95a1511dc423875b
Nguyễn Ngọc Thắng <thang5878@gmail.com>
2023-12-24 21:13:50 +0700

    Quartz sync: Dec 24, 2023, 9:13 PM

--- content/index.md
+++ content/index.md
@@ -1,4 +1,4 @@
-#tic_tac_toe_project 
+# Tic-tac-toe Project
 
 ## Related
 - [Hiển thị danh sách những người chơi online khác](Hiển%20thị%20danh%20sách%20những%20người%20chơi%20online%20khác.md) 
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Thang Nguyen's Digital Garden",
+    pageTitle: "Tic-tac-toe Project",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
